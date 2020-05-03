233d99c509d1028e85faeeedd5d2c0bfbd31274b
choikh0423 <kyuhwanchoi0423@gmail.com>
2020-05-03 12:39:48 +0900

    Implemented first example API View

--- backend/InstagramRepl/InstagramRepl/settings.py
+++ backend/InstagramRepl/InstagramRepl/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'newsfeed.apps.NewsfeedConfig',
-    'user.apps.UserConfig',
+    'newsfeed',
+    'user',
 ]
 
 MIDDLEWARE = [
---
+++ backend/InstagramRepl/user/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+import user.views
+
+urlpatterns = [
+    path('', user.views.ExampleView().as_view(), name="example")
+]
