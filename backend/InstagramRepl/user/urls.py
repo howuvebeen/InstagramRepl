176ff1c00281c7f6b0fc9d2c2120a75f05dc4166
choikh0423 <kyuhwanchoi0423@gmail.com>
2020-05-04 10:35:14 +0900

    Initiated AuthView

--- backend/InstagramRepl/user/urls.py
+++ backend/InstagramRepl/user/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 import user.views
 
 urlpatterns = [
-    path('', user.views.ExampleView().as_view(), name="example")
+    path('', user.views.AuthView().as_view(), name="authentication")
 ]
--- backend/InstagramRepl/user/views.py
+++ backend/InstagramRepl/user/views.py
@@ -1,3 +1,5 @@
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -8,4 +10,17 @@ from rest_framework.response import Response
 class ExampleView(APIView):
     def get(self, request):
         content = {'message': 'Hello, world'}
+        return Response(content)
+
+
+class AuthView(APIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, format=None):
+        content = {
+            # `django.contrib.auth.User` instance.
+            'user': unicode(request.user),
+            'auth': unicode(request.auth),  # None
+        }
         return Response(content)
