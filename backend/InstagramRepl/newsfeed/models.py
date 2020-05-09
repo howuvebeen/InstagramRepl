a40ee578aa3565409c5eb6a5c4d2f94495129120
Dongho Kim <dhkim0067@gmail.com>
2020-05-09 01:47:28 -0400

    Add Models to models.py
    Implemented models Profile, Post, Comment, and Like

--- backend/InstagramRepl/newsfeed/models.py
+++ backend/InstagramRepl/newsfeed/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+from InstagramRepl.user.models import Profile
+
 # Create your models here.
+
+class Post(models.Model):
+    owner = models.ForeignKey(Profile, on_delete= models.CASCADE)
+    photo = models.ImageField(required= True)
+    description = models.CharField(max_length= 250)
+
+class Comment(models.Model):
+    text = models.CharField(max_length= 250)
+    owner = models.ForeignKey(Profile, on_delete= models.CASCADE)
+    post = models.ForeignKey(Post, on_delete= models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+class Like(models.Model):
+    owner = models.OneToOneField()
+    post = models.ForeignKey(Post, on_delete= models.CASCADE, null = True)
+    
+    
--- backend/InstagramRepl/user/models.py
+++ backend/InstagramRepl/user/models.py
@@ -2,3 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    Following = models.ForeignKey(User, on_delete=models.CASCADE, null= True)
+    Followers = models.ForeignKey(User, on_delete=models.CASCADE, null= True)
+    profile_photo = models.ImageField(null= True)
+    DOB = models.DateField()
+
+    def __str__(self):
+        return user.username
