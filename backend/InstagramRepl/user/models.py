036f05b852d43bf72fc1cb215dedf554a55517a6
choikh0423 <kyuhwanchoi0423@gmail.com>
2020-05-09 16:05:47 +0900

    Model Error Fix

--- backend/InstagramRepl/user/models.py
+++ backend/InstagramRepl/user/models.py
@@ -3,12 +3,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Profile(models.Model):
-    
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    Following = models.ForeignKey(User, on_delete=models.CASCADE, null= True)
-    Followers = models.ForeignKey(User, on_delete=models.CASCADE, null= True)
-    profile_photo = models.ImageField(null= True)
+    Following = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, related_name="following")
+    Followers = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, related_name="follower")
+    profile_photo = models.ImageField(null=True)
     DOB = models.DateField()
 
     def __str__(self):
