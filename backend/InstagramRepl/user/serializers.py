665bb3c48e7670de92def03bed2b44be86450c65
Dongho Kim <dhkim0067@gmail.com>
2020-05-11 00:32:45 -0400

    Implement profile serializer

--- backend/InstagramRepl/user/serializers.py
+++ backend/InstagramRepl/user/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
 
+from django.contrib.auth.models import User
 from user.models import Profile
+from newsfeed.models import Post, Comment, Like
 
-class ProfileSerializer(serializers.ProfileSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    profiles = serializers.PrimaryKeyRelatedField(many= True, queryset = Profile.objects.all())
+    posts = serializers.PrimaryKeyRelatedField(many= True, queryset = Post.objects.all())
+    comments = serializers.PrimaryKeyRelatedField(many= True, queryset = Comment.objects.all())
+    likes = serializers.PrimaryKeyRelatedField(many= True, queryset = Like.objects.all())
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'profiles', 'posts', 'comments', 'likes']
+
+class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ['user', 'Following', 'Followers', 'profile_photo', 'DOB']
+        fields = ['owner', 'Following', 'Followers', 'profile_photo', 'DOB']
