84adbd9ac2b6d0bc00ff632858f7e8ae456e0f11
choikh0423 <kyuhwanchoi0423@gmail.com>
2020-05-11 13:57:22 +0900

    Resolved merge conflict

--- backend/InstagramRepl/user/models.py
+++ backend/InstagramRepl/user/models.py
@@ -13,7 +13,7 @@ class Profile(models.Model):
         "self", related_name="follower", blank=True)
     profile_photo = models.ImageField(
         default='uploads/profile/defaultimage.png', upload_to='uploads/profile/', blank=True)
-    DOB = models.DateField(default=datetime.date.today)
+    DOB = models.DateField(default=datetime.date.today, blank=True, null=True)
 
     def __str__(self):
         return self.user.username
