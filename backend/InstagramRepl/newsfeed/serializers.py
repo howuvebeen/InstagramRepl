966dc1e11e20a750acd7ce2313a65aa227ec5336
choikh0423 <kyuhwanchoi0423@gmail.com>
2020-05-11 13:11:58 +0900

    Modify Postserializer to have full link to photo

--- backend/InstagramRepl/newsfeed/serializers.py
+++ backend/InstagramRepl/newsfeed/serializers.py
@@ -5,24 +5,30 @@ from newsfeed.models import Post, Comment, Like
 
 
 class UserSerializer(serializers.ModelSerializer):
-    posts = serializers.PrimaryKeyRelatedField(many= True, queryset = Post.objects.all())
-    comments = serializers.PrimaryKeyRelatedField(many= True, queryset = Comment.objects.all())
-    likes = serializers.PrimaryKeyRelatedField(many= True, queryset = Like.objects.all())
+    posts = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Post.objects.all())
+    comments = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Comment.objects.all())
+    likes = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Like.objects.all())
 
     class Meta:
         model = User
         fields = ['id', 'username', 'posts', 'comments', 'likes']
 
+
 class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ['owner', 'photo', 'description']
+        fields = ['author', 'photo', 'description']
+
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ['text', 'owner', 'post']
+
 
 class LikeSerializer(serializers.ModelSerializer):
     class Meta:
