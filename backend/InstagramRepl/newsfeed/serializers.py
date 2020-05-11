e3ea255f106e0551489bbc5ac917c13bfde89c9a
Dongho Kim <dhkim0067@gmail.com>
2020-05-11 00:37:19 -0400

    Update newsfeed serializers.py
    Updated UserSerializer profile field to solve merge conflict

--- backend/InstagramRepl/newsfeed/serializers.py
+++ backend/InstagramRepl/newsfeed/serializers.py
@@ -2,16 +2,18 @@ from rest_framework import serializers
 
 from django.contrib.auth.models import User
 from newsfeed.models import Post, Comment, Like
+from user.models import Profile
 
 
 class UserSerializer(serializers.ModelSerializer):
+    profiles = serializers.PrimaryKeyRelatedField(many = True, queryset = Profile.objects.all())
     posts = serializers.PrimaryKeyRelatedField(many= True, queryset = Post.objects.all())
     comments = serializers.PrimaryKeyRelatedField(many= True, queryset = Comment.objects.all())
     likes = serializers.PrimaryKeyRelatedField(many= True, queryset = Like.objects.all())
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'posts', 'comments', 'likes']
+        fields = ['id', 'username', 'profiles', 'posts', 'comments', 'likes']
 
 class PostSerializer(serializers.ModelSerializer):
 
